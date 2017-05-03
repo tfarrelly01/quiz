d3923dd120dc771cfdf39c726b48d7fffa624164
Tony Farrelly <tfarrelly01@gmail.com>
2017-05-03 14:35:04 +0100

    version prior to styling

--- quiz/main.js
+++ quiz/main.js
@@ -1,35 +1,42 @@
+
+// Set the totalScore variable to 0 and insert it into the DOM and display on the web page using jQuery
+var totalScore = 0;
+$("#total #score").html(totalScore);
+// JavaScript method in line below
+// document.getElementById("score").innerHTML = totalScore;
+
+$(document).ready(function() {
+
+	// When the correct-ans class is clicked
+	$(".correct-ans").click(function(){
+		// Remove btn-default class and add btn-success
+		$(this).removeClass("btn-default").addClass("btn-success");
+
+        // Stop jquery doing anything else to this question
+		$(this).unbind();
+		$(this).siblings().unbind();
+
+        // Add 1 to totalScore and insert it into the DOM and display on the web page using jQuery
+        totalScore += 1;
+        $("#total #score").html(totalScore);
+        // JavaScript method in line below
+        //  document.getElementById("score").innerHTML = totalScore;
+
+    });
 /*
-<script>
- $(document).ready(function(){
-    $("#hide").click(function(){
-        $("p").hide();
+use toggle to reveal a message if Correct or Incorrect  !!!!!
+    $(".correct-ans").click(function(){
+        $("button").after("<p>Correct Answer</p>");
     });
-    $("#show").click(function(){
-        $("p").show();
-    });
-});
-</script>
-<p>If you click on the "Hide" button, I will disappear.</p>
-
-<button id="hide">Hide</button>
-<button id="show">Show</button>
 */
-$(document).ready(function() {
-    $("#Gready").fadeOut(2000);
-});
+    $(".wrong-ans").click(function(){
+	// Remove btn-default class and add btn-warning
+	    $(this).removeClass("btn-default").addClass("btn-danger");
 
-
-$(document).ready(function() {
-    $("#GSet").fadeOut(5000);
-});
-
-
-$(document).ready(function() {
-    $("#Go").fadeOut(10000);
-});
-
-$(document).ready(function(){
-    $("#reveal").click(function(){
-        $("#q1-answers").toggle();
+        // Stop jquery doing anything else to this question
+        $(this).unbind();
+		$(this).siblings().unbind();
     });
 });
+
+
